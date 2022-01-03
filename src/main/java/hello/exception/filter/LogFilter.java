0a0e31d96e9b5ee4ad34955f7e13f3f901aff51d
mnngy <59257197+mnngy@users.noreply.github.com>
2022-01-03 17:23:02 +0900

    서블릿 예외 처리 - 인터셉터

--- src/main/java/hello/exception/filter/LogFilter.java
+++ src/main/java/hello/exception/filter/LogFilter.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
